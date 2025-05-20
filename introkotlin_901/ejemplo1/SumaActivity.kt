91f3adfb07ea488a41230391fb888d52c5d6350b
Beatriz Ximena Alvarez Mena <ximenaward@gmail.com>
2025-05-19 23:17:25 -0600

    Ejemplo2

--- introkotlin_901/ejemplo1/SumaActivity.kt
+++ introkotlin_901/ejemplo1/SumaActivity.kt
@@ -3,30 +3,25 @@ package com.negabytes.introkotlin_901.ejemplo1
 import android.os.Bundle
 import android.widget.EditText
 import android.view.View
-import android.widget.RadioGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.introkotlin_901.R
+import com.negabytes.introkotlin_901.R
 
 class SumaActivity : AppCompatActivity() {
     private lateinit var et1: EditText
     private lateinit var et2: EditText
     private lateinit var tv1: TextView
-    private lateinit var rgOperaciones: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_suma)
-
-        et1 = findViewById(R.id.et1)
-        et2 = findViewById(R.id.et2)
-        tv1 = findViewById(R.id.tv1)
-        rgOperaciones = findViewById(R.id.rgOperaciones)
+        et1= findViewById(R.id.et1)
+        et2= findViewById(R.id.et2)
+        tv1= findViewById(R.id.tv1)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -35,32 +30,14 @@ class SumaActivity : AppCompatActivity() {
         }
     }
 
-    fun calcular(view: View) {
-        val num1 = et1.text.toString().toDoubleOrNull()
-        val num2 = et2.text.toString().toDoubleOrNull()
+    fun calcular(view:View?){
+        val num1=et1.text.toString().toDouble()
+        val num2=et1.text.toString().toDouble()
+        val suma=num1+num2
+        tv1.text="Resultado= $suma"
 
-        if (num1 == null || num2 == null) {
-            Toast.makeText(this, "Ingrese ambos números", Toast.LENGTH_SHORT).show()
-            return
-        }
 
-        val resultado = when (rgOperaciones.checkedRadioButtonId) {
-            R.id.rbSuma -> num1 + num2
-            R.id.rbResta -> num1 - num2
-            R.id.rbMultiplicacion -> num1 * num2
-            R.id.rbDivision -> {
-                if (num2 == 0.0) {
-                    Toast.makeText(this, "No se puede dividir por 0", Toast.LENGTH_SHORT).show()
-                    return
-                }
-                num1 / num2
-            }
-            else -> {
-                Toast.makeText(this, "Seleccione una operación", Toast.LENGTH_SHORT).show()
-                return
-            }
-        }
-
-        tv1.text = "Resultado = $resultado"
     }
+
+
 }
